e945d16709c64a2c24d157765602ceb69a6f1865
HamzaSaeed7 <maqwebdev.pro@gmail.com>
2026-08-06 13:01:55 +0500

    add the title to minute sheet

--- minutesheet/Data/Migrations/20260806071746_AddTitleToMinuteSheet.Designer.cs
+++ minutesheet/Data/Migrations/20260806071746_AddTitleToMinuteSheet.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using minutesheet.Data;
 
 #nullable disable
 
-namespace minutesheet.Migrations
+namespace minutesheet.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260806071746_AddTitleToMinuteSheet")]
+    partial class AddTitleToMinuteSheet
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
